8888903d3334408d6de10d036f653a46daa03554
Harlan Howe <harlan.howe@kinkaid.org>
2026-02-01 17:18:17 -0600

    user cash and start button displayed in top panel.

--- src/TDFrame.java
+++ src/TDFrame.java
@@ -8,8 +8,12 @@ public class TDFrame extends JFrame implements ActionListener
 {
     private TDPanel mainPanel;
     private JButton[] turretButtons;
+    private int userCash;
+    private JLabel cashLabel;
+    private JButton startButton;
 
     public int NUM_TURRET_TYPES = 3;
+    public final int[] PRICES = {100, 150, 200};
 
     public TDFrame()
     {
@@ -18,6 +22,7 @@ public class TDFrame extends JFrame implements ActionListener
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setLayout(new BorderLayout());
+        userCash = 1000;
         mainPanel = new TDPanel(this);
         getContentPane().add(mainPanel, BorderLayout.CENTER);
         buildTopPane();
@@ -26,20 +31,28 @@ public class TDFrame extends JFrame implements ActionListener
     public void buildTopPane()
     {
         Box topPanel = Box.createHorizontalBox();
-        turretButtons = new JButton[NUM_TURRET_TYPES];
-
+        topPanel.add(Box.createHorizontalStrut(1));
+        cashLabel = new JLabel("$"+userCash);
+        topPanel.add(cashLabel);
         topPanel.add(Box.createHorizontalGlue());
+
+        turretButtons = new JButton[NUM_TURRET_TYPES];
         for (int i=0; i<NUM_TURRET_TYPES; i++)
         {
             turretButtons[i] = buildTurretButton(i);
-            turretButtons[i].setToolTipText("Range: "+Turret.RANGES[i]+" Recharge: "+Turret.RECHARGE_TIMES[i]+" Damage: "+Turret.DAMAGES[i]);
-            turretButtons[i].addActionListener(this);
             topPanel.add(turretButtons[i]);
             if (i < NUM_TURRET_TYPES -1)
                 topPanel.add(Box.createHorizontalStrut(30));
         }
         topPanel.add(Box.createHorizontalGlue());
-
+        startButton = new JButton("Start");
+        startButton.setBackground(new Color(0,128,0));
+        startButton.setOpaque(true);
+        startButton.setBorderPainted(false);
+        startButton.setForeground(Color.WHITE);
+        startButton.addActionListener(this);
+        topPanel.add(startButton);
+        topPanel.add(Box.createHorizontalStrut(1));
         getContentPane().add(topPanel, BorderLayout.NORTH);
     }
 
@@ -52,8 +65,12 @@ public class TDFrame extends JFrame implements ActionListener
         localGraphics.setColor(Color.LIGHT_GRAY);
         localGraphics.fillRect(0,0,40,40);
         temp.drawSelf(localGraphics);
-
-        return new JButton(new ImageIcon(bi));
+        JButton result = new JButton("$"+PRICES[type], new ImageIcon(bi));
+        result.setToolTipText("Range: "+Turret.RANGES[type]+" Recharge: "+Turret.RECHARGE_TIMES[type]+" Damage: "+Turret.DAMAGES[type]);
+        result.addActionListener(this);
+        result.setHorizontalTextPosition(SwingConstants.CENTER);
+        result.setVerticalTextPosition(SwingConstants.BOTTOM);
+        return result;
     }
 
     @Override
@@ -63,6 +80,8 @@ public class TDFrame extends JFrame implements ActionListener
         {
             if (e.getSource() == turretButtons[i])
                 System.out.println("User pressed button "+i+".");
+            if (e.getSource() == startButton)
+                System.out.println("User pressed start.");
         }
     }
 }
