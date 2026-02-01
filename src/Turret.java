e8cc3320e7ed656c24a3b6ea7f33a9af95689fe1
Harlan Howe <harlan.howe@kinkaid.org>
2026-02-01 09:13:05 -0600

    Turret now has third form; isolated BasicStrokes into static variables.

--- src/Turret.java
+++ src/Turret.java
@@ -10,6 +10,9 @@ public class Turret
     private double myTimeSinceLastFire;
     private int[] myLoc;
 
+    private static BasicStroke thinLine;
+    private static BasicStroke thickLine;
+
     public Turret(int range, double period, int damage, int type, World myWorld)
     {
         this.myRange = range;
@@ -21,6 +24,11 @@ public class Turret
         myLoc = new int[2];
         myLoc[0] = -100;
         myLoc[1] = -100;
+
+        if (null == thickLine)
+            thinLine = new BasicStroke(1);
+        if (null == thickLine)
+            thickLine = new BasicStroke(2);
     }
 
     public void setMyLoc(int[] myLoc)
@@ -46,11 +54,21 @@ public class Turret
                 break;
             case 1:
                 g.setColor(Color.BLACK);
-                ((Graphics2D)g).setStroke(new BasicStroke(2));
+                ((Graphics2D)g).setStroke(thickLine); // use a thicker pen.
                 g.drawOval(myLoc[0]-18, myLoc[1]-18, 36, 36);
                 g.drawLine(myLoc[0]-12, myLoc[1]-12, myLoc[0]+12, myLoc[1]+12);
                 g.drawLine(myLoc[0]-12, myLoc[1]+12, myLoc[0]+12, myLoc[1]-12);
-                ((Graphics2D)g).setStroke(new BasicStroke(1));
+                ((Graphics2D)g).setStroke(thinLine); // restore the pen to thin for the next thing we draw.
+                break;
+            case 2:
+            default:
+                g.setColor(Color.BLACK);
+                int[] x_vals = {myLoc[0]-18, myLoc[0]-9, myLoc[0]+9, myLoc[0]+18,
+                myLoc[0]+9, myLoc[0]-9, myLoc[0]-18};
+                int[] y_vals = {myLoc[1], myLoc[1]-18, myLoc[1]-18, myLoc[1], myLoc[1]+18, myLoc[1]+18, myLoc[1]};
+                g.drawPolygon(x_vals, y_vals, 7);
+                for (int i=0; i<3; i++)
+                    g.drawLine(x_vals[i], y_vals[i], x_vals[i+3], y_vals[i+3]);
         }
 
     }
